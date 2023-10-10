a5bb518eae12aab2ec87f8b7e9ea1817e6f489f6
Omar-G26 <omargarcia0826@gmail.com>
2023-10-10 13:55:39 -0700

    db

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sleeptrackerapp/Pages/Main/LoginPage.dart';
+import 'package:sleeptrackerapp/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future<void> main() async{
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+   options: DefaultFirebaseOptions.currentPlatform,
+ );
+ runApp(const MyApp());
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
