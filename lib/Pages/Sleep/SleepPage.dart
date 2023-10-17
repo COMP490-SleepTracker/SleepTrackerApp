b0a3d7d98b8af286956b69e6288d31ef3284a864
Sean <aracitdev@gmail.com>
2023-10-17 16:15:46 -0700

    Initial light sensor and accelerometer reading.

--- lib/Pages/Sleep/SleepPage.dart
+++ lib/Pages/Sleep/SleepPage.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:light/light.dart';
+
 import 'package:sleeptrackerapp/Pages/NavigationPanel.dart';
 
 class SleepPage extends StatefulWidget {
@@ -10,16 +16,68 @@ class SleepPage extends StatefulWidget {
 }
 
 class SleepPageState extends State<SleepPage> {
+
+  List<double>? _accelerometerValues; //accelerometer values
+  Light? _light; //light sensor
+  int? _luxValue; //light value
+
+
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[]; //stream subscription
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(
+      userAccelerometerEvents.listen(
+        (UserAccelerometerEvent event) {
+          setState(() {
+            _accelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support Accelerometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+
+    //subscribe to light sensor
+    _light = Light();
+    try {
+    _streamSubscriptions.add(_light!.lightSensorStream.listen((luxValue) {
+      setState(() {
+        _luxValue = luxValue;
+      });
+    }));
+    } on LightException catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final userAccelerometer = _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Sleep'),
       ),
       drawer : const NavigationPanel(),
-      body: const Center(
-        child: Text(
-          'Sleep',
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('User Accelerometer: ${userAccelerometer ?? 'Not Available'}'),
+            Text('Light: ${_luxValue ?? 'Not Available'}'),
+          ],
         ),
       ),
     );
