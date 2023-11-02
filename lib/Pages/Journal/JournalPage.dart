9b2de004d7ad2e69a9822cfac6206d738fee8e7a
perezjav325 <perezjav325@gmail.com>
2023-11-02 12:59:59 -0700

    Added Calendar view and TextField on Journal Page

--- lib/Pages/Journal/JournalPage.dart
+++ lib/Pages/Journal/JournalPage.dart
@@ -3,6 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:sleeptrackerapp/Model/AuthenticationManager.dart';
 import 'package:sleeptrackerapp/Pages/Main/LoginPage.dart';
 import 'package:sleeptrackerapp/Pages/NavigationPanel.dart';
+import 'package:calendar_timeline/calendar_timeline.dart';
 
 class JournalPage extends StatefulWidget {
   const JournalPage({super.key, required this.title});
@@ -13,6 +14,17 @@ class JournalPage extends StatefulWidget {
 }
 
 class _JournalPageState extends State<JournalPage> {
+  late DateTime _selectedDate;
+
+   @override
+  void initState() {
+    super.initState();
+    _resetSelectedDate();
+  }
+
+  void _resetSelectedDate() {
+    _selectedDate = DateTime.now();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,14 +40,63 @@ class _JournalPageState extends State<JournalPage> {
         title: Text(widget.title),
       ),
       drawer : const NavigationPanel(),
-      body: const Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('Journal Page'),
+            const Padding(
+              padding: EdgeInsets.all(5),
+            ),
+            CalendarTimeline(
+              showYears: true,
+              initialDate: _selectedDate,
+              firstDate: DateTime.now(),
+              lastDate: DateTime.now().add(const Duration(days: 365 * 4)),
+              onDateSelected: (date) => setState(() => _selectedDate = date),
+              leftMargin: 10,
+              monthColor: Colors.white70,
+              dayColor: Colors.white70,
+              dayNameColor: const Color(0xFF333A47),
+              activeDayColor: Colors.white,
+              activeBackgroundDayColor: Colors.purple[200],
+              dotsColor: const Color(0xFF333A47),
+              selectableDayPredicate: (date) => date.day != 23,
+              locale: 'en',
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                ),
+                child: const Text(
+                  'Today',
+                  style: TextStyle(color: Color(0xFF333A47)),
+                ),
+                onPressed: () => setState(() => _resetSelectedDate()),
+              ),
+            ),
+           Container(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+              constraints: const BoxConstraints(
+                maxHeight: 85,
+              ),
+              child: const SingleChildScrollView(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Log notes on your sleep or dreams'
+                ),
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                maxLines: null,
+    ),
+  ),
+)
           ],
         ),
-      ),
-    );
+      )
+        );
   }
 }
