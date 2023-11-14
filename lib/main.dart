71809251ebc524db963f80bf6fccd881cea6cc61
Sean <aracitdev@gmail.com>
2023-11-14 15:32:52 -0800

    Added a local storage and settings store.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:sleeptrackerapp/Pages/Main/LoginPage.dart';
 import 'package:sleeptrackerapp/Model/SleepDataManager.dart';
 import 'package:get_it/get_it.dart';
 import 'Model/AuthenticationManager.dart';
+import 'Model/SettingsManager.dart';
 
 void main() {
   GetIt.instance.registerSingleton<AuthenticationManager>(TestAuthenticationManagerImpl());
   GetIt.instance.registerSingleton<SleepDataManager>(TestSleepDataManagerImpl());
+  GetIt.instance.registerSingleton<SettingsManager>(SettingsManager());
   runApp(const MyApp());
 }
 
