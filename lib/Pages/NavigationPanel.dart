f871d076186547a830235ff40df4bc1c31663127
Riley Espiritu <riley.espiritu@gmail.c>
2024-03-05 14:31:23 -0800

    Added TipsPage and edited NavigationPanel

--- lib/Pages/NavigationPanel.dart
+++ lib/Pages/NavigationPanel.dart
@@ -5,6 +5,7 @@ import 'package:sleeptrackerapp/Pages/Settings/SettingsPage.dart';
 import 'package:sleeptrackerapp/Pages/Sleep/SleepPage.dart';
 import 'package:sleeptrackerapp/Pages/Statistics/StatisticsPage.dart';
 import 'package:sleeptrackerapp/Pages/Journal/JournalPage.dart';
+import 'package:sleeptrackerapp/Pages/Tips/TipsPage.dart';
 
 
 class NavigationPanel extends Drawer
@@ -13,9 +14,9 @@ class NavigationPanel extends Drawer
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> pages = [const MainPage(title: 'Sleep Tracker+'), const SleepPage(title: 'Sleep'), const StatisticsPage(title: 'Statistics'), const JournalPage(title: 'Journal'), const SettingsPage(title: 'Settings')];
-    List<String> titles = ['Sleep Tracker+', 'Sleep', 'Statistics', 'Journal', 'Settings'];
-    List<Icon> icons = [const Icon(Icons.home), const Icon(Icons.bedtime), const Icon(Icons.bar_chart), const Icon(Icons.book), const Icon(Icons.settings)];
+    List<Widget> pages = [const MainPage(title: 'Sleep Tracker+'), const SleepPage(title: 'Sleep'), const StatisticsPage(title: 'Statistics'), const JournalPage(title: 'Journal'), const SettingsPage(title: 'Settings'), const TipsPage(title:'Tips')];
+    List<String> titles = ['Sleep Tracker+', 'Sleep', 'Statistics', 'Journal', 'Settings', 'Tips'];
+    List<Icon> icons = [const Icon(Icons.home), const Icon(Icons.bedtime), const Icon(Icons.bar_chart), const Icon(Icons.book), const Icon(Icons.settings), const Icon(Icons.lightbulb)];
     List<Widget> navigationTiles = [];
     for (int i = 0; i < pages.length; i++)
     {
