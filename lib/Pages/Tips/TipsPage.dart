1f71279f4e4d5313ea7998d541a184b79fbf41e5
nolanmendoza2 <nolan.mendoza.128@my.csun.edu>
2024-03-28 14:41:07 -0700

    Added logic to calculate sleep Score Sleep score color changes based on value Populates health tip

--- lib/Pages/Tips/TipsPage.dart
+++ lib/Pages/Tips/TipsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:health/health.dart';
 import 'package:sleeptrackerapp/Pages/NavigationPanel.dart';
 
 class TipsPage extends StatelessWidget {
@@ -7,9 +8,73 @@ class TipsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Define the maximum sleep score
+    const double maxSleepScore = 110.0;
+    
+// Assuming you have fetched the health data for steps, deep, rem, and sleeptime
+  double stepsValue = 4583; // Example value for steps from 11.28
+  double deepValue = 41; // Example value for deep sleep from 11.28
+  double remValue = 69; // Example value for REM sleep from 11.28
+  double sleeptimeValue = 241; // Example value for sleep time from 11.28
+
+  // Define the multipliers
+  double stepsMultiplier = 0.005;
+  double deepMultiplier = 0.25;
+  double remMultiplier = 0.25;
+  double sleeptimeMultiplier = 0.05;
+
+  // Perform mathematical operations on the health data using multipliers
+  int stepsmath = (stepsValue * stepsMultiplier).toInt();
+  int deepmath = (deepValue * deepMultiplier).toInt();
+  int remmath = (remValue * remMultiplier).toInt();
+  int sleeptimemath = (sleeptimeValue * sleeptimeMultiplier).toInt();
+  
+ 
+
+  int currentSleepScore =(stepsmath + deepmath + remmath + sleeptimemath);
+    // Calculate the scaled sleep score percentage
+    final double sleepScorePercentage = currentSleepScore / maxSleepScore;
+
+    // Retrieve the color of the circular progress indicator
+    String sleepQuality = '';
+    Color progressColor = Colors.green;
+
+    // Determine sleep quality and progress color
+    if (currentSleepScore >= 89) {
+      sleepQuality = 'Great';
+      progressColor = Colors.green;
+    } else if (currentSleepScore >= 68) {
+      sleepQuality = 'Good';
+      progressColor = Colors.yellow;
+    } else if (currentSleepScore >= 47) {
+      sleepQuality = 'Okay';
+      progressColor = Colors.orange;
+    } else {
+      sleepQuality = 'Bad';
+      progressColor = Colors.red;
+    }
+
+    String tipTitle = '';
+    String tipDescription = '';
+
+    // Determine which tip to display based on the current sleep score
+    if (currentSleepScore >= 89) {
+      tipTitle = 'Tip 1';
+      tipDescription = 'INSERT "GREAT" SLEEP SCORE HEALTH TIP HERE';
+    } else if (currentSleepScore >= 68) {
+      tipTitle = 'Tip 2';
+      tipDescription = 'INSERT "GOOD" SLEEP SCORE HEALTH TIP HERE';
+    } else if (currentSleepScore >= 47) {
+      tipTitle = 'Tip 3';
+      tipDescription = 'INSERT "OKAY" SLEEP SCORE HEALTH TIP HERE';
+    } else {
+      tipTitle = 'Tip 4';
+      tipDescription = 'INSERT "BAD" SLEEP SCORE HEALTH TIP HERE';
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tips'),
+        title: const Text('Tips'),
       ),
       drawer: const NavigationPanel(),
       body: Column(
@@ -33,28 +98,34 @@ class TipsPage extends StatelessWidget {
                 Container(
                   width: 100.0,
                   height: 100.0,
-                  child: const Stack(
+                  child: Stack(
                     alignment: Alignment.center,
                     children: [
                       SizedBox(
                         width: 120.0, // Size of the circular progress indicator
                         height: 120.0,
-                        child: CircularProgressIndicator(
-                          value: 0.85, // Dummy sleep score value (85%)
-                          backgroundColor: Colors.white,
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-                          strokeWidth: 20.0,
+                        child: CustomCircularProgressIndicator(
+                          value: sleepScorePercentage, // Use scaled sleep score percentage
+                          progressColor: progressColor,
                         ),
                       ),
                       Text(
-                        '85', // Dummy sleep score value
+                        currentSleepScore.toStringAsFixed(0), // Display the current sleep score
                         style: TextStyle(
-                          fontSize: 48.0,
+                          fontSize: 45.0,
                           fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                          color: progressColor, // Use the same color as the progress indicator
                         ),
                       ),
                     ],
+                  ),
+                ),
+                Text(
+                  sleepQuality, // Display sleep quality
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: progressColor, // Use the same color as the progress indicator
                   ),
                 ),
               ],
@@ -62,25 +133,37 @@ class TipsPage extends StatelessWidget {
           ),
           Expanded(
             child: ListView(
-              children: const [
+              children: [
                 TipCard(
-                  title: 'Tip 1',
-                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                  title: tipTitle,
+                  description: tipDescription,
                 ),
-                TipCard(
-                  title: 'Tip 2',
-                  description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                ),
-                TipCard(
-                  title: 'Tip 3',
-                  description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                ),
-                // Add more TipCards as needed
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class CustomCircularProgressIndicator extends StatelessWidget {
+  final double value;
+  final Color progressColor;
+
+  const CustomCircularProgressIndicator({
+    Key? key,
+    required this.value,
+    required this.progressColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CircularProgressIndicator(
+      value: value,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      valueColor: AlwaysStoppedAnimation<Color>(progressColor),
+      strokeWidth: 13.0,
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_core
 import firebase_database
 import flutter_local_notifications
 import flutter_secure_storage_macos
+import google_sign_in_ios
 import just_audio
 import path_provider_foundation
 import shared_preferences_foundation
@@ -24,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
